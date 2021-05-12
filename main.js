c241c1f74455fa59308960f0106ff6c2efbd7930
parnasos <lauragraizer@gmail.com>
2021-05-12 11:05:18 +0200

    Update js

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+const button = document.querySelector(".js-button");
+const numberUser = document.querySelector(".js-yourNumber");
+const Clue = document.querySelector(".js-clue");
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+const numberRandom = parseInt(getRandomNumber(100));
+console.log(numberRandom);
+
+const userValue = parseInt(numberUser.value);
+console.log(userValue);
+
+const messageClue = Clue.value;
+
+function play(event) {
+  event.preventDefault();
+  if (userValue === numberRandom) {
+    messageClue.innerHTML = "Has ganado campeona!!!";
+  } else if (userValue > numberRandom) {
+    messageClue.innerHTML = "Demasiado alto";
+  } else if (userValue < numberRandom) {
+    messageClue.innerHTML = "Demasiado alto";
+  } else {
+    messageClue.innerHTML = "El número debe estar entre 1 y 100";
+  }
+}
+
+button.addEventListener("click", play);
